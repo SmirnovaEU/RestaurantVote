5cc5dc22147dfe20a978f762adfb3dffa56dd426
SmirnovaEU <62511446+SmirnovaEU@users.noreply.github.com>
2021-01-18 17:26:09 +0300

    fix admin rest controller delete test

--- src/main/java/ru/topjava/voting/web/user/ProfileRestController.java
+++ src/main/java/ru/topjava/voting/web/user/ProfileRestController.java
@@ -1,33 +1,20 @@
 package ru.topjava.voting.web.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import ru.topjava.voting.model.User;
-import ru.topjava.voting.repository.UserRepository;
 import ru.topjava.voting.to.UserTo;
 import ru.topjava.voting.web.AuthorizedUser;
 
 import static ru.topjava.voting.util.ValidationUtil.assureIdConsistent;
 import static ru.topjava.voting.web.SecurityUtil.authUserId;
 
-//@Service("userService")
-//@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 @RestController
 @RequestMapping(ProfileRestController.REST_URL)
-public class ProfileRestController extends AbstractUserController {// implements UserDetailsService {
+public class ProfileRestController extends AbstractUserController {
     static final String REST_URL = "/rest/profile";
-
-    @Autowired
-    private UserRepository repository;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get() {
@@ -46,14 +33,4 @@ public class ProfileRestController extends AbstractUserController {// implements
         assureIdConsistent(userTo, authUser.getId());
         super.update(userTo);
     }
-
-//    @Override
-//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//
-//        User user = repository.getByEmail(email.toLowerCase());
-//        if (user == null) {
-//            throw new UsernameNotFoundException("User " + email + " is not found");
-//        }
-//        return new AuthorizedUser(user);
-//    }
 }
