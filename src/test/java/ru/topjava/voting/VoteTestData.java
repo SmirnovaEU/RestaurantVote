5a23da78659180449e44c498e7ce779198f36713
SmirnovaEU <62511446+SmirnovaEU@users.noreply.github.com>
2021-01-21 12:50:12 +0300

    vote test

--- src/test/java/ru/topjava/voting/VoteTestData.java
+++ src/test/java/ru/topjava/voting/VoteTestData.java
@@ -4,12 +4,13 @@ import ru.topjava.voting.model.Restaurant;
 import ru.topjava.voting.model.Vote;
 
 import java.time.Month;
+import java.util.List;
 
 import static java.time.LocalDate.of;
 import static ru.topjava.voting.model.AbstractBaseEntity.START_SEQ;
 
 public class VoteTestData {
-    public static final TestMatcher<Vote> VOTE_MATCHER = TestMatcher.usingIgnoringFieldsComparator(Vote.class, "user");
+    public static final TestMatcher<Vote> VOTE_MATCHER = TestMatcher.usingIgnoringFieldsComparator(Vote.class, "user", "rest");
 
     public static final int VOTE1_ID = START_SEQ + 5;
     public static final int REST1_ID = START_SEQ + 2;
@@ -19,9 +20,15 @@ public class VoteTestData {
     public static final Restaurant rest3 = new Restaurant(REST1_ID + 2, "Papa Jones");
 
     public static final Vote vote1 = new Vote(VOTE1_ID, rest1, of(2020, Month.JANUARY, 30));
+    public static final Vote vote2 = new Vote(VOTE1_ID + 1, rest2, of(2020, Month.JANUARY, 31));
+    public static final Vote vote3 = new Vote(VOTE1_ID + 2, rest3, of(2020, Month.JANUARY, 30));
+    public static final Vote vote4 = new Vote(VOTE1_ID + 3, rest1, of(2020, Month.JANUARY, 31));
+    public static final Vote vote5 = new Vote(VOTE1_ID + 4, rest2, of(2021, Month.JANUARY, 9));
+
+    public static final List<Vote> userVotes = List.of(vote5, vote2, vote1);
 
     public static Vote getNew() {
-        return new Vote(null, rest3, of(2020, Month.FEBRUARY, 30));
+        return new Vote(null, null, of(2020, Month.FEBRUARY, 28));
     }
 
     public static Vote getUpdated() {
--- src/test/java/ru/topjava/voting/web/vote/VoteRestControllerTest.java
+++ src/test/java/ru/topjava/voting/web/vote/VoteRestControllerTest.java
@@ -1,27 +1,78 @@
 package ru.topjava.voting.web.vote;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.topjava.voting.model.Vote;
+import ru.topjava.voting.repository.VoteRepository;
 import ru.topjava.voting.web.AbstractControllerTest;
+import ru.topjava.voting.web.json.JsonUtil;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static ru.topjava.voting.VoteTestData.VOTE_MATCHER;
-import static ru.topjava.voting.VoteTestData.vote1;
+import static ru.topjava.voting.TestUtil.readFromJson;
+import static ru.topjava.voting.TestUtil.userHttpBasic;
+import static ru.topjava.voting.UserTestData.USER_ID;
+import static ru.topjava.voting.VoteTestData.*;
+import static ru.topjava.voting.UserTestData.user;
 
 class VoteRestControllerTest extends AbstractControllerTest {
 
     private static final String REST_URL = VoteRestController.REST_URL + '/';
 
+    @Autowired
+    private VoteRepository voteRepository;
+
     @Test
     void getByDate() throws Exception {
-        perform(MockMvcRequestBuilders.get(REST_URL + "by?date=" + vote1.getDate()))
+        perform(MockMvcRequestBuilders.get(REST_URL + "by?date=" + vote1.getDate())
+                .with(userHttpBasic(user)))
                 .andExpect(status().isOk())
                 .andDo(print())
                 // https://jira.spring.io/browse/SPR-14472
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(VOTE_MATCHER.contentJson(vote1));
+    }
+
+    @Test
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL)
+                .with(userHttpBasic(user)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(VOTE_MATCHER.contentJson(userVotes));
+    }
+
+    @Test
+    void create() throws Exception {
+        Vote newVote = getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newVote))
+                .with(userHttpBasic(user)));
+
+        Vote created = readFromJson(action, Vote.class);
+        int newId = created.id();
+        newVote.setId(newId);
+        VOTE_MATCHER.assertMatch(created, newVote);
+        VOTE_MATCHER.assertMatch(voteRepository.get(newId, USER_ID), newVote);
+    }
+
+    @Test
+    void update() throws Exception {
+        Vote updated = getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL + "/change")
+                .param("date", vote1.getDate().toString())
+                .param("restId", Integer.toString(REST1_ID))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated))
+                .with(userHttpBasic(user)))
+                .andExpect(status().isNoContent());
+
+        VOTE_MATCHER.assertMatch(voteRepository.get(VOTE1_ID, USER_ID), updated);
     }
 }
